d4152767dc9dcdff5bba004a16bc2ea3e55a91d1
sid96020 <sid9602044416@gmail.com>
2021-12-30 12:57:19 +0530

    file_upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var MongoClient = require('mongodb').MongoClient;
+const express = require("express");
+const app = express();
+var url = "mongodb://localhost:27017/";
+let data;
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  const users = require("./user.json");
+  dbo.collection("customers").insertMany(users, function(err, res) {
+    if (err) throw err;
+
+    console.log("Number of documents inserted: " + res.insertedCount);
+  })
+  dbo.collection("customers").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    data=result;
+    db.close();
+  });
+});
+app.listen(5000, () => {
+    console.log(`Server is up and running on 5000 ...`);
+  });
+  app.get("/", (req, res) => {
+    res.send(data);
+});
